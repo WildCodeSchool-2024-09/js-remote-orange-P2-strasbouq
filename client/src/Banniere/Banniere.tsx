af2ca844545184f8518e8af9147faa840819ec3f
Badre-s <bsahili@hotmail.fr>
2024-11-12 12:34:02 +0100

    UPD conflict resolved

--- client/src/Banniere/Banniere.tsx
+++ client/src/Banniere/Banniere.tsx
@@ -1,18 +1,16 @@
 import ban from "../assets/logo/ban.jpg";
+import "../styles/Banniere.css";
 
 const Banniere = () => {
-	return (
-		<div>
-			{}
-			<img src={ban} alt="Bannière" />
-
-			{}
-			<div>
-				<h2>Nos Bouquets</h2>
-				<button type="button">Découvrir</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="banniere">
+      <img src={ban} alt="Bannière" className="banniere-img" />
+      <div className="banniere-content">
+        <h2>Nos Bouquets</h2>
+        <button type="button">Découvrir</button>
+      </div>
+    </div>
+  );
 };
 
 export default Banniere;
