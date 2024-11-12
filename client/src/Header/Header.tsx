f99d6c4545b9dbdde38710bbc6b2c2ebc0e2d2f7
Badre-s <bsahili@hotmail.fr>
2024-11-12 12:39:41 +0100

    UPD conflict resolved

--- client/src/Header/Header.tsx
+++ client/src/Header/Header.tsx
@@ -2,19 +2,19 @@ import facebook from "../assets/logo/facebook.png";
 import instagram from "../assets/logo/instagram.png";
 import logo from "../assets/logo/logo.png";
 import panier from "../assets/logo/panier.png";
-import "../styles/header.css";
+import "./Header.css";
 
 const Header = () => {
-  return (
-    <>
-      <header>
-        <img className="fb" src={facebook} alt="Logo Facebook" />
-        <img className="insta" src={instagram} alt="Logo Instagram" />
-        <img className="stasbouq" src={logo} alt="Logo Strasbouq" />
-        <img className="pan" src={panier} alt="Logo Panier" />
-      </header>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<img className="fb" src={facebook} alt="Logo Facebook" />
+				<img className="insta" src={instagram} alt="Logo Instagram" />
+				<img className="stasbouq" src={logo} alt="Logo Strasbouq" />
+				<img className="pan" src={panier} alt="Logo Panier" />
+			</header>
+		</>
+	);
 };
 
 export default Header;
